b1f8cf69554c8edf19788c720c719d54816344e8
Eric Rincon <royeric77@gmail.com>
2023-01-27 19:54:10 -0600

    more done

--- app/routes.py
+++ app/routes.py
@@ -6,49 +6,67 @@
 from flask import render_template, request, redirect, url_for, session
 from app import app
 
+app.secret_key = 'random_secret_key'
+
+products = [
+    {'id': 1, 'name': 'Product 1', 'price':9.99},
+    {'id': 2, 'name': 'Product 2', 'price':9.99},
+    {'id': 3, 'name': 'Product 3', 'price':9.99}
+]
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
         session['name'] = name
         return redirect(url_for('products'))
-    return render_template('index.html')
+    return render_template('index.html', product=products)
 
 @app.route('/allproducts')
 def Show_all_products():
     name = session.get('name')
     return render_template('products.html', name=name)
 
-@app.route('/allproducts')
-def ShowAllProducts():
+@app.route('/product/<int:product_id>')
+def singleproduct(product_id):
+    product = next(p for p in products if p['id'] == product_id)
+    return render_template('product.html', product=product)
 
+def add_to_cart():
+    if 'username' not in session:
+        return "You must be logged in to add items to your cart"
 
-@app.route('/product1')
-def ShowProduct1():
+    product_id = request.form['product_id']
+    product = next(p for p in products if p['id'] == int(product_id))
 
-@app.route('/product2')
-def ShowProduct2():
+    if 'cart' not in session:
+        session['cart'] = []
 
-@app.route('/product3')
-def ShowProduct3():
+    session['cart'].append(product)
 
-@app.route('/product4')
-def ShowProduct4():
+    return "Product added to cart"
 
-@app.route('/product5')
-def ShowProduct5():
-
-@app.route('/product6')
-def ShowProduct6():
 
 @app.route('/cart')
 def ShowCart():
-    return render_template('cart.html')
+    if 'cart' not in session:
+        return "Your cart is empty"
+    
+    total_price = sum(item['price'] for item in session['cart'])
 
-@app.route('/removefromcart')
-def Remove_from_cart():
+    return render_template('cart.html', cart=session['cart'], total_price = total_price)
+
+@app.route('/remove_all_items')
+def remove_all_items():
+    session.pop('cart', None)
     return redirect(url_for('cart'))
-#Also add a button to just remove 1 item from the cart
+
+@app.route('/remove_item/<int:product_id>')
+def remove_item(product_id):
+    cart = session['cart']
+    session['cart'] = [item for item in cart if item['id'] != product_id]
+    return redirect(url_for('cart'))
+
 
 
 
