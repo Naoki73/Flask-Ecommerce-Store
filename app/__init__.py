537d2bc005055ddaee90576bc22e19f1f4a599a0
Eric Rincon <royeric77@gmail.com>
2023-01-28 20:15:13 -0600

    message

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from config import Config
 from flask_migrate import Migrate
-from flask_login import LoginManager
 from .auth.routes import auth
 
 app = Flask(__name__)
 
-login_manager = LoginManager(app)
+# login_manager = LoginManager(app)
 
-login_manager.login_view ='auth.loginPage'
+# login_manager.login_view ='auth.loginPage'
 
 app.register_blueprint(auth)
 
